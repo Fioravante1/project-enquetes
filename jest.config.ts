9d6e5f7c4616b136d98da527bdd13249fde36ad0
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2022-01-10 18:03:41 -0300

    chore: add ts-node

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,12 @@
 export default {
+  preset: 'ts-jest',
   roots: [
     '<rootDir>/src'
   ],
+  verbose: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
