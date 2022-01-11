6ac22990dc2d23ff1782264fb2e17aea7d5437a3
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2022-01-11 20:02:04 -0300

    refactor: add Error personalizado

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,19 +1,14 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { ParamError } from '../errors/param-error'
+import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new ParamError('name')
-      }
+      return badRequest(new ParamError('name'))
     }
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new ParamError('email')
-      }
+      return badRequest(new ParamError('email'))
     }
     return {
       statusCode: 200,
