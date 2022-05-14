4d04c247c7114417168e8353e2f0592477c1be26
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2022-05-13 23:22:24 -0300

    refactor: move server error to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,9 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { ParamError } from '../errors/param-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { InvalidParamError } from '../errors/invalid-param-error'
-import { ServerError } from '../errors/server-error'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -32,11 +31,7 @@ export class SignUpController implements Controller {
         body: 'Tudo ok'
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-
-      }
+      return serverError()
     }
   }
 }
