926a53c765d158bff5805788a52abd2c6773fe74
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2022-05-18 21:40:13 -0300

    test: ensure signupcontroller returns 400 if password confirmation fails

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -68,6 +68,21 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new ParamError('password'))
   })
 
+  test('Should return 400 if no confirmation fails', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_password',
+        passwordConfirmation: 'invalid_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'))
+  })
+
   test('Should return 400 if no password confirmation is provided', () => {
     const { sut } = makeSut()
     const httpRequest = {
