8e2f531ac80eef1aa6f5c7865cc72802acb1e1c0
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2022-05-19 21:49:31 -0300

    chore: add jest configs and scripts

---
+++ jest-integration-config.js
@@ -0,0 +1,3 @@
+const config = require('./jest.config')
+config.testMath = ['**/*.test.ts']
+module.export = config
---
+++ jest-unit-config.js
@@ -0,0 +1,3 @@
+const config = require('./jest.config')
+config.testMath = ['**/*.spec.ts']
+module.export = config
